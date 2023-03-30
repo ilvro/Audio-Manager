4368c2ebc55a2fea9b7ed74abc3f7d28c2cb8155
ilvro <cildumyt@gmail.com>
2023-03-30 15:29:12 -0300

    added youtube link downloader for mp3s

--- pages/linkInput.xaml.cs
+++ pages/linkInput.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MediaToolkit;
+using MediaToolkit.Model;
+using VideoLibrary;
+using Path = System.IO.Path;
 
 namespace Audio_Controller.pages
 {
@@ -26,12 +31,40 @@ namespace Audio_Controller.pages
 
         private void LinkDownloadBtn_Click(object sender, RoutedEventArgs e)
         {
+            string uploadFolder = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "\\tracks\\";
+            string videoUrl = URLTextBox.Text;
+            if (videoUrl.Contains("youtube.com") && videoUrl.Contains("/watch?v="))
+            {
+                downloadVideo(uploadFolder, videoUrl);
+            }
             this.Close();
         }
 
         private void URLTextBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             URLTextBox.Text = "";
+        }
+
+        private void downloadVideo(string uploadFolder, string url) // this is kind of slow - there might be a better way to do this
+        {
+            var youtube = YouTube.Default;
+            var vid = youtube.GetVideo(url);
+            string videopath = Path.Combine(uploadFolder, vid.FullName);
+            File.WriteAllBytes(videopath, vid.GetBytes());
+
+            var inputFile = new MediaFile { Filename = Path.Combine(uploadFolder, vid.FullName) };
+            var outputFile = new MediaFile { Filename = Path.Combine(uploadFolder, $"{vid.FullName}.mp3") };
+
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+
+                engine.Convert(inputFile, outputFile);
+            }
+
+
+            File.Delete(Path.Combine(uploadFolder, vid.FullName));
         }
     }
 }
