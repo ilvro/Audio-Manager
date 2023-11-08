9c67d290393aef6cfa66944647de43873c3c6576
ilvro <114951562+ilvro@users.noreply.github.com>
2023-11-08 18:26:56 -0300

    fixed formatting

--- classes/ListViewStyle.cs
+++ classes/ListViewStyle.cs
@@ -19,19 +19,12 @@ namespace Audio_Controller.classes
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 ProgressBar progressBar = (ProgressBar)sender;
-                Song song = (Song)progressBar.DataContext; // Get the Song instance.
-
-                // Now you have the correct songPlayer associated with the Song.
+                Song song = (Song)progressBar.DataContext;
                 SongPlayer songPlayer = song.songPlayer;
 
-                // Calculate the position where the user clicked as a percentage of the total width
                 double clickPosition = e.GetPosition(progressBar).X / progressBar.ActualWidth;
-
-                // Update the song's playback position based on the click position
                 double newPlaybackPosition = clickPosition * songPlayer.CurrentSong.TotalDuration;
                 song.SetPlaybackPosition(TimeSpan.FromSeconds(newPlaybackPosition));
-
-                // Update the MediaPlayer's position
                 songPlayer.mediaPlayer.Position = TimeSpan.FromSeconds(newPlaybackPosition);
             }
         }
