fe5971f71de106e5f67069447d642556ef1a44e6
ilvro <114951562+ilvro@users.noreply.github.com>
2023-11-15 21:43:57 -0300

    removed useless partial class

--- classes/Song.cs
+++ classes/Song.cs
@@ -131,27 +131,5 @@ namespace Audio_Controller.classes
         {
             playbackPosition = position;
         }
-
-        public class SmoothProgressBar
-        {
-            public static double GetSmoothValue(DependencyObject obj)
-            {
-                return (double)obj.GetValue(SmoothValueProperty);
-            }
-
-            public static void SetSmoothValue(DependencyObject obj, double value)
-            {
-                obj.SetValue(SmoothValueProperty, value);
-            }
-
-            public static readonly DependencyProperty SmoothValueProperty =
-                DependencyProperty.RegisterAttached("SmoothValue", typeof(double), typeof(SmoothProgressBar), new PropertyMetadata(0.0, changing));
-
-            private static void changing(DependencyObject d, DependencyPropertyChangedEventArgs e)
-            {
-                var anim = new DoubleAnimation((double)e.OldValue, (double)e.NewValue, new TimeSpan(0, 0, 0, 0, 250));
-                (d as ProgressBar).BeginAnimation(ProgressBar.ValueProperty, anim, HandoffBehavior.Compose);
-            }
-        }
     }
 }
